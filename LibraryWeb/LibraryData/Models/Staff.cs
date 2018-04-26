a616e76a54f5a1f9d6fd5c8793cf1c10ed233833
suraj kumar <skumar565@csc.com>
2018-04-26 17:55:01 +0530

    models added
    models added

---
+++ LibraryWeb/LibraryData/Models/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData.Models
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public string StaffName { get; set; }
+    }
+}
