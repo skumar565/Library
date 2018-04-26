828e1158837bf14be69dd8bc87a7c182ddfb26f6
suraj kumar <skumar565@csc.com>
2018-04-26 18:07:43 +0530

    admin file
    admin cs file added

---
+++ LibraryWeb/LibraryData/Models/admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData.Models
+{
+    class admin
+    {
+        public int adminId { get; set; }
+        public string adminName { get; set; }
+    }
+}
